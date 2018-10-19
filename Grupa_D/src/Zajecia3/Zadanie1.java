856e3055045ce9fb39ace35d258a8f32949a45ad
P Marecki <pmarecki.pro@gmail.com>
2018-10-19 17:41:05 +0200

    Gra w chińczyka

--- Grupa_D/src/Zajecia3/Zadanie1.java
+++ Grupa_D/src/Zajecia3/Zadanie1.java
@@ -7,6 +7,12 @@ public class Zadanie1 {
         // --> sprawdzić czy da się ułożyć trójkąt z bokami o długości a,b,c
         //zadanie3: z liczb z przedziału [1..100] znaleźć takie a,b,c:
         //  a*a + b*b == c*c
+        //zadanie4: obecny czas zadany jest w postaci dwóch liczb całkowitych,
+        // hh (godzina) i mm (minuta); np. int hh=17; int mm=15 odpowiadają 17:15;
+        // podany jest tez odcinek czasu `int delta`; jest to liczba minut,
+        // które chcemy dodać do obecnego czasu; proszę wyliczyć
+        // godzinę i minutę chwili powstałej po dodaniu `delta` do obecnego czasu.
+
 
         //kod1:
         long suma = 0;
