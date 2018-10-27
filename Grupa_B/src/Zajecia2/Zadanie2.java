1a25787ebca2eb0efc0fa898f193ad4fa259ca76
Stasszek <staszek906@o2.pl>
2018-10-27 14:05:17 +0200

    tabliczka mnozenia

---
+++ Grupa_B/src/Zajecia2/Zadanie2.java
@@ -0,0 +1,13 @@
+package Zajecia2;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 11; i++) {
+            //System.out.print(i + " ");
+            for (int j = 1; j < 11; j++){
+                System.out.print(i * j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
